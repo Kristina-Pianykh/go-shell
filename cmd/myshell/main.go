e58236da5068978c896fd5110d5c3420621b57b2
Kristina Pianykh <kristinavrnrus@gmail.com>
2025-01-09 14:32:45 +0100

    Add REPL

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -9,14 +9,15 @@ import (
 var _ = fmt.Fprint
 
 func main() {
-	// Uncomment this block to pass the first stage
-	fmt.Fprint(os.Stdout, "$ ")
 
 	// Wait for user input
-	input, err := bufio.NewReader(os.Stdin).ReadString('\n')
-	if err != nil {
-		panic(err)
+	for {
+		fmt.Fprint(os.Stdout, "$ ")
+		input, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		if err != nil {
+			panic(err)
+		}
+		cmd := input[:len(input)-1]
+		fmt.Fprintf(os.Stdout, fmt.Sprintf("%s: command not found\n", cmd))
 	}
-	cmd := input[:len(input)-1]
-	fmt.Fprintf(os.Stdout, fmt.Sprintf("%s: command not found\n", cmd))
 }
